f3607e73664546cddfd8bd4d7d8e0240a717cc84
javahongxi <javahongxi@qq.com>
2020-10-31 11:11:55 +0800

    object_storage

--- object_storage/chapter2/apiServer/apiServer.go
+++ object_storage/chapter2/apiServer/apiServer.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/javahongxi/golab/object_storage/chapter2/apiServer/heartbeat"
+	"github.com/javahongxi/golab/object_storage/chapter2/apiServer/locate"
+	"github.com/javahongxi/golab/object_storage/chapter2/apiServer/objects"
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	go heartbeat.ListenHeartbeat()
+	http.HandleFunc("/objects/", objects.Handler)
+	http.HandleFunc("/locate/", locate.Handler)
+	log.Fatal(http.ListenAndServe(os.Getenv("LISTEN_ADDRESS"), nil))
 }
