26b0f34e57c4bb8e5f1c66682822ee9c2f5b0562
javahongxi <javahongxi@qq.com>
2020-11-17 11:25:21 +0800

    embed

---
+++ lang/embed/entry.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/javahongxi/golab/lang/tree"
+)
+
+type myTreeNode struct {
+	*tree.Node
+}
+
+func (myNode *myTreeNode) postOrder() {
+	if myNode == nil || myNode.Node == nil {
+		return
+	}
+
+	left := myTreeNode{myNode.Left}
+	right := myTreeNode{myNode.Right}
+
+	left.postOrder()
+	right.postOrder()
+	myNode.Print()
+}
+
+func main() {
+	root := myTreeNode{&tree.Node{Value: 3}}
+	root.Left = &tree.Node{}
+	root.Right = &tree.Node{5, nil, nil}
+	root.Right.Left = new(tree.Node)
+	root.Left.Right = tree.CreateNode(2)
+	root.Right.Left.SetValue(4)
+
+	fmt.Print("In-order traversal: ")
+	root.Traverse()
+
+	fmt.Print("My own post-order traversal: ")
+	root.postOrder()
+	fmt.Println()
+
+	nodeCount := 0
+	root.TraverseFunc(func(node *tree.Node) {
+		nodeCount++
+	})
+	fmt.Println("Node count:", nodeCount)
+
+	c := root.TraverseWithChannel()
+	maxNodeValue := 0
+	for node := range c {
+		if node.Value > maxNodeValue {
+			maxNodeValue = node.Value
+		}
+	}
+	fmt.Println("Max node value:", maxNodeValue)
+}
