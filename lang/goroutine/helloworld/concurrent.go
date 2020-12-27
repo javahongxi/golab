c5c3bdd55e9be62ea019288746ef0accb781767f
javahongxi <javahongxi@qq.com>
2020-12-28 05:13:33 +0800

    hello world

---
+++ lang/goroutine/helloworld/concurrent.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan string)
+	for i := 0; i < 5000; i++ {
+		go printHelloWorld(i, ch)
+	}
+	for {
+		msg := <-ch
+		fmt.Println(msg)
+	}
+}
+
+func printHelloWorld(i int, ch chan string) {
+	for {
+		ch <- fmt.Sprintf("Hello world from goroutine %d\n", i)
+	}
+}
--- lang/http/server/server.go
+++ lang/http/server/server.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello, world!\n")
+func HelloServer(writer http.ResponseWriter, request *http.Request) {
+	io.WriteString(writer, "Hello, world!\n")
 }
 
 func main() {
 	http.HandleFunc("/hello", HelloServer)
+	http.HandleFunc("/hello2", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "<h1>Hello world %s!</h1>", request.FormValue("name"))
+	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
